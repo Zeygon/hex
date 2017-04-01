68c5c746e4da8696fb43ae71f157e12cbadd1e7e
Zeygon <luca.dev@outlook.com>
2017-04-02 01:24:35 +0200

    Not working :(

--- app.js
+++ app.js
@@ -48,6 +48,17 @@ angular.module('formApp', ['ngAnimate', 'ui.router','webcam'])
             templateUrl: 'partials/step-4-map.html',
             controller: function($scope) {
               $scope.c = "#fc0061";
+
+              var map = L.map('map').setView([51.505, -0.09], 13);
+
+              L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              }).addTo(map);
+
+              L.marker([51.5, -0.09]).addTo(map)
+                  .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+                  .openPopup();
+                  
             }
         })
 
